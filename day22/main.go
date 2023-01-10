0e74f2e77c27d6521b71c1e50078ae27a96be917
David Ellis <davide@splunk.com>
2023-01-10 16:20:12 -0700

    initial changes for part2

--- day22/main.go
+++ day22/main.go
@@ -17,6 +17,12 @@ const (
 	LEFT
 )
 
+const (
+	CLOCKWISE rotation = iota
+	COUNTERCLOCKWISE
+	NOROTATION
+)
+
 type pos struct {
 	x int
 	y int
@@ -29,14 +35,22 @@ type player struct {
 
 type heading int8
 
+// rowDescr describes the min/max range for valid x values in the row
+// and what next position and heading to go to if you walk out of range
 type rowDescr struct {
-	minX int
-	maxX int
+	minX     int
+	minXNext player
+
+	maxX     int
+	maxXNext player
 }
 
 type colDescr struct {
-	minY int
-	maxY int
+	minY     int
+	minYNext player
+
+	maxY     int
+	maxYNext player
 }
 
 // a freespace is true if a player can stand there
@@ -52,7 +66,7 @@ type command string
 
 type subcommand struct {
 	moves int
-	dir   heading
+	dir   rotation
 }
 type rotation int8
 
@@ -133,6 +147,18 @@ func parseInput(lines []string) (grid, command) {
 		g.cols[x] = cd
 	}
 
+	//Part1
+	for y := range g.rows {
+		rd := &g.rows[y]
+		rd.maxXNext = player{loc: pos{rd.minX, y}, dir: RIGHT} //same dir, but start over at MinX
+		rd.minXNext = player{loc: pos{rd.maxX, y}, dir: LEFT}
+	}
+	for x := range g.cols {
+		cd := &g.cols[x]
+		cd.maxYNext = player{loc: pos{x, cd.minY}, dir: DOWN} //same dir, but start over at MinX
+		cd.minYNext = player{loc: pos{x, cd.maxY}, dir: UP}
+	}
+
 	cmd := command(lines[len(lines)-1])
 	return g, cmd
 }
@@ -154,10 +180,10 @@ func max(a, b int) int {
 func (c command) RunCommand(g *grid, start player) (spaces int, final player) {
 	cur := start
 	for _, sc := range c.GetSubcommands() {
+		cur.dir = cur.dir.Rotate(sc.dir)
 
 		for i := 0; i < sc.moves; i++ {
-			cur.dir = sc.dir
-			cur.loc.NextStep(sc.dir, g)
+			cur.NextStep(g)
 
 		}
 
@@ -170,8 +196,7 @@ func (c command) GetSubcommands() []subcommand {
 	ret := make([]subcommand, 0)
 
 	//First Move has no rotation
-	dir := RIGHT
-	sc := subcommand{moves: 0, dir: dir}
+	sc := subcommand{moves: 0, dir: NOROTATION}
 	i := strings.IndexAny(string(c), "LR")
 	v, err := strconv.Atoi(string(c)[:i])
 	if err != nil {
@@ -187,12 +212,11 @@ func (c command) GetSubcommands() []subcommand {
 		}
 
 		if nextSCStart := strings.IndexAny(str[1:], "LR"); nextSCStart == -1 {
-			ret = append(ret, parseSubcommand(str, dir))
+			ret = append(ret, parseSubcommand(str))
 			str = "" //We're Done
 		} else {
 			nextSCStart++
-			sc := parseSubcommand(str[:nextSCStart], dir)
-			dir = sc.dir
+			sc := parseSubcommand(str[:nextSCStart])
 			ret = append(ret, sc)
 			str = str[nextSCStart:]
 		}
@@ -201,13 +225,13 @@ func (c command) GetSubcommands() []subcommand {
 	return ret
 }
 
-func parseSubcommand(str string, prev heading) subcommand {
+func parseSubcommand(str string) subcommand {
 	ret := subcommand{}
 	switch str[0] {
 	case 'L':
-		ret.dir = prev.Rotate(false)
+		ret.dir = COUNTERCLOCKWISE
 	case 'R':
-		ret.dir = prev.Rotate(true)
+		ret.dir = CLOCKWISE
 	default:
 		panic("boo")
 	}
@@ -220,58 +244,51 @@ func parseSubcommand(str string, prev heading) subcommand {
 	return ret
 }
 
-func (h heading) Rotate(clockwise bool) heading {
-	if clockwise {
+func (h heading) Rotate(dir rotation) heading {
+	switch dir {
+	case NOROTATION:
+		return h
+	case CLOCKWISE:
 		n := (h + 1) % 4
 		return n
+	case COUNTERCLOCKWISE:
+		n := h - 1
+		if n < 0 {
+			n = 3
+		}
+		return n
 	}
-
-	n := h - 1
-	if n < 0 {
-		n = 3
-	}
-	return n
+	panic("eek")
 }
 
-func (p *pos) NextStep(dir heading, g *grid) {
-	switch dir {
+func (p *player) NextStep(g *grid) {
+	next := *p
+	switch next.dir {
 	case UP:
-		nextY := p.y - 1
-		if nextY < g.cols[p.x].minY {
-			nextY = g.cols[p.x].maxY
+		next.loc.y--
+		if next.loc.y < g.cols[next.loc.x].minY {
+			next = g.cols[p.loc.x].minYNext
 		}
-
-		if g.spaces[nextY][p.x] {
-			p.y = nextY
-		}
-
 	case DOWN:
-		nextY := p.y + 1
-		if nextY > g.cols[p.x].maxY {
-			nextY = g.cols[p.x].minY
-		}
-
-		if g.spaces[nextY][p.x] {
-			p.y = nextY
+		next.loc.y++
+		if next.loc.y > g.cols[next.loc.x].maxY {
+			next = g.cols[next.loc.x].maxYNext
 		}
 	case LEFT:
-		nextX := p.x - 1
-		if nextX < g.rows[p.y].minX {
-			nextX = g.rows[p.y].maxX
-		}
-
-		if g.spaces[p.y][nextX] {
-			p.x = nextX
+		next.loc.x--
+		if next.loc.x < g.rows[next.loc.y].minX {
+			next = g.rows[next.loc.y].minXNext
 		}
 	case RIGHT:
-		nextX := p.x + 1
-		if nextX > g.rows[p.y].maxX {
-			nextX = g.rows[p.y].minX
+		next.loc.x++
+		if next.loc.x > g.rows[next.loc.y].maxX {
+			next = g.rows[next.loc.y].maxXNext
 		}
+	}
 
-		if g.spaces[p.y][nextX] {
-			p.x = nextX
-		}
+	//If next position isn't blocked, go there, and update heading if we've walked onto another face
+	if g.spaces[next.loc.y][next.loc.x] {
+		*p = next
 	}
 }
 
--- day22/main_test.go
+++ day22/main_test.go
@@ -7,13 +7,18 @@ import (
 )
 
 func TestRotate(t *testing.T) {
-	assert.Equal(t, DOWN, RIGHT.Rotate(true))
-	assert.Equal(t, LEFT, DOWN.Rotate(true))
-	assert.Equal(t, UP, LEFT.Rotate(true))
-	assert.Equal(t, RIGHT, UP.Rotate(true))
+	assert.Equal(t, DOWN, RIGHT.Rotate(CLOCKWISE))
+	assert.Equal(t, LEFT, DOWN.Rotate(CLOCKWISE))
+	assert.Equal(t, UP, LEFT.Rotate(CLOCKWISE))
+	assert.Equal(t, RIGHT, UP.Rotate(CLOCKWISE))
 
-	assert.Equal(t, UP, RIGHT.Rotate(false))
-	assert.Equal(t, RIGHT, DOWN.Rotate(false))
-	assert.Equal(t, DOWN, LEFT.Rotate(false))
-	assert.Equal(t, LEFT, UP.Rotate(false))
+	assert.Equal(t, UP, RIGHT.Rotate(COUNTERCLOCKWISE))
+	assert.Equal(t, RIGHT, DOWN.Rotate(COUNTERCLOCKWISE))
+	assert.Equal(t, DOWN, LEFT.Rotate(COUNTERCLOCKWISE))
+	assert.Equal(t, LEFT, UP.Rotate(COUNTERCLOCKWISE))
+
+	assert.Equal(t, UP, UP.Rotate(NOROTATION))
+	assert.Equal(t, DOWN, DOWN.Rotate(NOROTATION))
+	assert.Equal(t, RIGHT, RIGHT.Rotate(NOROTATION))
+	assert.Equal(t, LEFT, LEFT.Rotate(NOROTATION))
 }
