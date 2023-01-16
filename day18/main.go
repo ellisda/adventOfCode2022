4d8b82a57ee609d5e82124fd11139bae5319da30
David Ellis <davide@splunk.com>
2023-01-16 13:20:14 -0700

    cleanup

--- day18/main.go
+++ day18/main.go
@@ -81,57 +81,52 @@ func (s surface) IsEmpty(p pos) bool {
 	return true
 }
 
-func (s surface) OutwardSurfaces() int {
-	ret := 0
-	// p0, p1 := s.BoundingBox()
-	// yz := NewGrid(p1.y-p0.y+1, p1.z-p0.z+1)
-	// for x := p0.x; x <= p1.x; x++ {
-	// 	for y := p0.y; y <= p1.y; y++ {
-
-	// 		minZ := math.MaxInt
-	// 		maxZ := math.MinInt
-
-	// 		for _, p := range s {
-	// 			if p.x == x && p.y == y {
-	// 				minZ = min(minZ, p.z)
-	// 				maxZ = max(maxZ, p.z)
-	// 			}
-	// 		}
-
-	// 		if minZ != math.MaxInt || maxZ != math.MinInt
-	// 		yzMaxX[y] = make([]int, p1.z-p0.z+1)
-	// 		for z := p0.z; z <= p1.z; z++ {
-
-	// 		}
-	// 	}
-	// }
-	// ret := 0
-	// for i := range s {
-	// 	for _, n := range s[i].Neighbors() {
-	// 		if s.IsEmpty(n) {
-	// 			ret++
-	// 		}
-	// 	}
-	// }
-	return ret
-}
-
 func (s surface) OutwardFacingSurfaces() int {
 	water := FillWithWater(s)
 	ret := 0
-	// debug := pos{2, 2, 1}
 	for i := range s {
-		// if s[i] == debug {
-		// 	fmt.Println("hey")
-		// }
 		for _, n := range s[i].Neighbors() {
 			if water[n] {
-				// fmt.Println("Lava", s[i], "touching water", n)
 				ret++
 			}
 		}
 	}
 	return ret
+}
+
+func FillWithWater(s surface) map[pos]bool {
+	p0, p1 := s.BoundingBox()
+	ret := make(map[pos]bool)
+
+	s.visit(p0, ret, p0, p1)
+	return ret
+}
+
+func (s surface) BoundingBox() (min, max pos) {
+	minAll := math.MaxInt
+	maxAll := math.MinInt
+	for i := range s {
+		minAll = Min(Min(Min(minAll, s[i].x), s[i].y), s[i].z)
+		maxAll = Max(Max(Max(maxAll, s[i].x), s[i].y), s[i].z)
+
+	}
+	minAll -= 1
+	maxAll += 1
+	return pos{minAll, minAll, minAll}, pos{maxAll, maxAll, maxAll}
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func (s surface) visit(p pos, water map[pos]bool, min, max pos) {
@@ -154,110 +149,4 @@ func inBounds(p, min, max pos) bool {
 		p.z < min.z || p.z > max.z
 
 	return !out
-}
-
-func FillWithWater(s surface) map[pos]bool {
-	p0, p1 := s.BoundingBox()
-	ret := make(map[pos]bool)
-
-	//alt - visit all via  dfs
-	s.visit(p0, ret, p0, p1)
-
-	// debug := pos{2, 2, 1}
-
-	for x := p0.x; x <= p1.x; x++ {
-		for y := p0.y; y <= p1.y; y++ {
-
-			// if x == debug.x && y == debug.y {
-			// 	fmt.Print("hel")
-			// }
-
-			for z := p0.z; z <= p1.z; z++ {
-				if !s.IsEmpty(pos{x, y, z}) {
-					break
-				}
-				ret[pos{x, y, z}] = true
-				// fmt.Println("filling with water", x, y, z)
-			}
-
-			for z := p1.z; z >= p0.z; z-- {
-				if !s.IsEmpty(pos{x, y, z}) {
-					break
-				}
-				ret[pos{x, y, z}] = true
-			}
-		}
-	}
-
-	for x := p0.x; x <= p1.x; x++ {
-		for z := p0.z; z <= p1.z; z++ {
-			for y := p0.y; y <= p1.y; y++ {
-				if !s.IsEmpty(pos{x, y, z}) {
-					break
-				}
-				ret[pos{x, y, z}] = true
-			}
-
-			for y := p1.y; y >= p0.y; y-- {
-				if !s.IsEmpty(pos{x, y, z}) {
-					break
-				}
-				ret[pos{x, y, z}] = true
-			}
-		}
-	}
-
-	for z := p0.z; z <= p1.z; z++ {
-		for y := p0.y; y <= p1.y; y++ {
-			for x := p0.x; x <= p1.x; x++ {
-				if !s.IsEmpty(pos{x, y, z}) {
-					break
-				}
-				ret[pos{x, y, z}] = true
-			}
-
-			for x := p1.x; x >= p0.x; x-- {
-				if !s.IsEmpty(pos{x, y, z}) {
-					break
-				}
-				ret[pos{x, y, z}] = true
-			}
-		}
-	}
-	return ret
-}
-
-func NewGrid(len1, len2 int) [][]bool {
-	ret := make([][]bool, len1)
-	for x := 0; x < len1; x++ {
-		ret[x] = make([]bool, len2)
-	}
-	return ret
-}
-
-func Min(a, b int) int {
-	if a < b {
-		return a
-	}
-	return b
-}
-
-func Max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func (s surface) BoundingBox() (min, max pos) {
-	minAll := math.MaxInt
-	maxAll := math.MinInt
-	for i := range s {
-		minAll = Min(Min(Min(minAll, s[i].x), s[i].y), s[i].z)
-		maxAll = Max(Max(Max(maxAll, s[i].x), s[i].y), s[i].z)
-
-	}
-	minAll -= 1
-	maxAll += 1
-	return pos{minAll, minAll, minAll}, pos{maxAll, maxAll, maxAll}
 }
